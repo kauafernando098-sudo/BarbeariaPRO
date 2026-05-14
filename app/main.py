563f495633d8a4f2abc1f8b5a031fe662e32cd33
unknown <kauafernando098@gmail.com>
2026-05-14 20:55:42 -0300

    Corrigindo rotas main

--- app/main.py
+++ app/main.py
@@ -1,65 +1,32 @@
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.base import Base
 from app.database.connection import engine
 
-from app.routes.agendamentos import router as agendamentos_router
 from app.routes.usuarios import router as usuarios_router
+from app.routes.agendamentos import router as agendamentos_router
 
-print("CORS ATIVO")
 app = FastAPI()
 
-
 app.add_middleware(
-
     CORSMiddleware,
-
     allow_origins=[
-
         "https://barbearia-pro-pi.vercel.app"
-
     ],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
 )
 
-
 Base.metadata.create_all(bind=engine)
 
-
-app.include_router(
-
-    agendamentos_router,
-
-    prefix="/agendamentos",
-
-    tags=["Agendamentos"]
-
-)
-
-
-app.include_router(
-
-    usuarios_router,
-
-    prefix="/usuarios",
-
-    tags=["Usuários"]
-
-)
-
+app.include_router(usuarios_router)
+app.include_router(agendamentos_router)
 
 @app.get("/")
 def home():
 
     return {
-
         "mensagem": "Servidor funcionando"
-
     }
