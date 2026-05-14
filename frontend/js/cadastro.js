4c2fff2c5d9af0bdd265d9ca9150c18aebf4b278
unknown <kauafernando098@gmail.com>
2026-05-14 19:49:16 -0300

    Corrigindo cadastro online

--- frontend/js/cadastro.js
+++ frontend/js/cadastro.js
@@ -1,43 +1,49 @@
 async function cadastrar() {
 
-    const username = document.getElementById("username").value
-    const senha = document.getElementById("senha").value
+    const username =
+        document.getElementById(
+            "username"
+        ).value
+
+    const senha =
+        document.getElementById(
+            "senha"
+        ).value
 
     const resposta = await fetch(
+
         "https://barbeariapro.onrender.com/usuarios/",
+
         {
+
             method: "POST",
 
             headers: {
-                "Content-Type": "application/json"
+
+                "Content-Type":
+                "application/json"
+
             },
 
             body: JSON.stringify({
-                username,
-                senha
+
+                username: username,
+
+                senha: senha,
+
+                role: "cliente"
+
             })
+
         }
+
     )
 
-    const dados = await resposta.json()
+    const dados =
+        await resposta.json()
 
-    console.log(resposta.status)
-    console.log(dados)
-
-    const mensagem = document.getElementById("mensagem")
-
-    if (resposta.ok) {
-
-        mensagem.style.color = "lightgreen"
-        mensagem.innerText = "Conta criada com sucesso"
-        console.log("REDIRECIONAR PARA LOGIN")
-        window.open("login.html", "_self")
-
-
-    } else {
-
-        mensagem.style.color = "red"
- 
-        mensagem.innerText = dados.detail
-    }
+    document.getElementById(
+        "mensagem"
+    ).innerText =
+    dados.mensagem || dados.detail
 }
