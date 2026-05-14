9b03cd08d4cebe9a2ada40719552ab89b146beb0
unknown <kauafernando098@gmail.com>
2026-05-14 19:53:45 -0300

    Corrigindo main

--- app/main.py
+++ app/main.py
@@ -1,23 +1,16 @@
 from fastapi import FastAPI
 
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.database.base import Base
 from app.database.connection import engine
-from app.routes import usuarios
-from app.routes import agendamentos
 
 from app.routes.agendamentos import router as agendamentos_router
 from app.routes.usuarios import router as usuarios_router
 
-from app.models.agendamento_model import AgendamentoModel
-from app.models.usuario_model import UsuarioModel
-
-
-from fastapi import FastAPI
-
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 
 app.add_middleware(
 
@@ -36,38 +29,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(
-    usuarios.router
-)
-
-app.include_router(
-    agendamentos.router
-)
-app.add_middleware(
-    CORSMiddleware,
-
-    allow_origins=["*"],
-
-    allow_credentials=True,
-
-    allow_methods=["*"],
-
-    allow_headers=["*"],
-)
 
 Base.metadata.create_all(bind=engine)
 
 
 app.include_router(
+
     agendamentos_router,
+
     prefix="/agendamentos",
+
     tags=["Agendamentos"]
+
 )
 
+
 app.include_router(
+
     usuarios_router,
+
     prefix="/usuarios",
+
     tags=["Usuários"]
+
 )
 
 
@@ -75,5 +59,7 @@ app.include_router(
 def home():
 
     return {
+
         "mensagem": "Servidor funcionando"
+
     }
