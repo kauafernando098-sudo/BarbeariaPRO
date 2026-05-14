3b3e7801da66402dcd7ce5983c34a581cdce5a7e
unknown <kauafernando098@gmail.com>
2026-05-14 20:39:18 -0300

    Corrigindo routes usuarios

--- app/routes/usuarios.py
+++ app/routes/usuarios.py
@@ -1,14 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
-
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.models.usuario_model import UsuarioModel
 from app.database.connection import get_db
 
 from app.models.usuario import Usuario
-
-from app.services.usuario_service import criar_usuario
 from app.models.login import Login
 
 from app.services.usuario_service import (
@@ -16,46 +11,24 @@ from app.services.usuario_service import (
     login_usuario
 )
 
-
-router = APIRouter()
+router = APIRouter(
+    prefix="/usuarios",
+    tags=["Usuários"]
+)
 
 
 @router.post("/")
-def criar_usuario(usuario, db):
+def criar(
+    usuario: Usuario,
+    db: Session = Depends(get_db)
+):
 
-    usuario_existente = db.query(
-        UsuarioModel
-    ).filter(
-        UsuarioModel.username == usuario.username
-    ).first()
-
-    if usuario_existente:
-
-        raise HTTPException(
-            status_code=400,
-            detail="Essa conta já existe"
-        )
-
-    senha_hash = bcrypt.hashpw(
-        usuario.senha.encode("utf-8"),
-        bcrypt.gensalt()
-    ).decode("utf-8")
-
-    novo_usuario = UsuarioModel(
-        username=usuario.username,
-        senha=senha_hash,
-        role=usuario.role
+    return criar_usuario(
+        usuario,
+        db
     )
 
-    db.add(novo_usuario)
 
-    db.commit()
-
-    db.refresh(novo_usuario)
-
-    return {
-        "mensagem": "Usuário criado com sucesso"
-    }
 @router.post("/login")
 def login(
     login: Login,
@@ -66,32 +39,3 @@ def login(
         login,
         db
     )
-
-
-def criar_usuario(usuario, db):
-
-    usuario_existente = db.query(
-        UsuarioModel
-    ).filter(
-        UsuarioModel.username == usuario.username
-    ).first()
-
-    if usuario_existente:
-
-        raise HTTPException(
-            status_code=400,
-            detail="Essa conta já existe"
-        )
-    if not usuario:
-        raise HTTPException(
-            status_code=401,
-            detail="Usuário não encontrado"
-        )
-    
-    if len(usuario.senha) < 6:
-        raise HTTPException(
-            status_code=400,
-            detail="A senha deve conter pelo menos 6 caracteres"
-        )
-
-  
